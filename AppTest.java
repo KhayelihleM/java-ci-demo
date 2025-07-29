da71b890b51bcbe0865e368de1320fd5e9227067
Khaya <khayahlanti@gmail.com>
2025-07-29 09:19:51 +0200

    add code to AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    public void testGreet() {
+        assertEquals("Hello, World!", App.greet("World"));
+    }
+}
